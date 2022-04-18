2ae7dcecfd6f72f07e3914612c7b00b96808e465
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2022-04-18 17:27:12 +0530

    Added pattern 2

---
+++ src/main/java/com/github/pepcoding/patterns/Pattern2.java
@@ -0,0 +1,17 @@
+package com.github.pepcoding.patterns;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*	");
+            }
+            System.out.println();
+        }
+    }
+}
