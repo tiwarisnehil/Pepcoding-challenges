cb0b26e3b6d73ea75344586f02c3c8791fdf906f
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2022-04-18 17:43:13 +0530

    Added pattern20

---
+++ src/main/java/com/github/pepcoding/patterns/Pattern9.java
@@ -0,0 +1,22 @@
+package com.github.pepcoding.patterns;
+
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if( i==j || i+j==n+1){
+                    System.out.print("*	");
+                }
+                else{
+                    System.out.print("	");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
