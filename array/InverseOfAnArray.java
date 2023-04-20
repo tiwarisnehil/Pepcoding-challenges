6ce131eb80b5d31dc42498bb81b9f2bcb0968e5d
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-04-20 18:54:29 +0530

    Add files via upload

---
+++ array/InverseOfAnArray.java
@@ -0,0 +1,39 @@
+package array;
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void display(int[] a) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int val : a) {
+            sb.append(val + " ");
+        }
+        System.out.println(sb);
+    }
+
+    public static int[] inverse  (int[] a) {
+        int [] in = new int [a.length];
+        for(int i=0;i<a.length;i++){
+            int temp= a[i];
+            in[temp]=i;
+        }
+        return in;
+    }
+
+    public static void InverseOfAnArray(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] inv = inverse(a);
+        display(inv);
+    }
+
+}
