1c8d404d213f2de5d204b61e392930002e748da4
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-04-23 14:02:07 +0530

    Add files via upload

---
+++ array/TheStateOfWakandaTwo.java
@@ -0,0 +1,28 @@
+package array;
+
+
+import java.io.*;
+import java.util.*;
+
+public class TheStateOfWakandaTwo {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int [][]arr=new int[n][n];
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+
+        for(int g=0;g<arr.length;g++){
+            for(int i=0,j=g;j<arr.length;i++,j++){
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+
+}
