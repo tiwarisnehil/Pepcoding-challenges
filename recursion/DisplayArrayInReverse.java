cd6c37f1f068fb4fdfd35217d56f7221e034af8c
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-04-28 22:06:18 +0530

    Add files via upload

---
+++ recursion/DisplayArrayInReverse.java
@@ -0,0 +1,29 @@
+package recursion;
+
+
+import java.io.*;
+import java.util.*;
+
+public class DisplayArrayInReverse {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        displayArrReverse(arr,0);
+    }
+
+    public static void displayArrReverse(int[] arr, int idx) {
+        if(idx==arr.length){
+            return;
+        }
+
+        displayArrReverse(arr,idx+1);
+        System.out.println(arr[idx]);
+    }
+
+}
