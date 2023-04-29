9f9145f771f5a2e145e2503cb9b9c1210cea83da
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-04-29 21:24:03 +0530

    Add files via upload

---
+++ recursion/LastIndex.java
@@ -0,0 +1,38 @@
+package recursion;
+
+import java.io.*;
+import java.util.*;
+
+public class LastIndex {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int x = scn.nextInt();
+        int li = lastIndex(arr, 0, x);
+        System.out.println(li);
+    }
+
+    public static int lastIndex(int[] arr, int idx, int x) {
+        if(idx==arr.length){
+            return -1;
+        }
+
+        int lissa=lastIndex(arr,idx+1,x);
+        if(lissa==-1){
+            if(arr[idx]==x){
+                return idx;
+            }else{
+                return -1;
+            }
+        }else{
+            return lissa;
+        }
+    }
+
+}
