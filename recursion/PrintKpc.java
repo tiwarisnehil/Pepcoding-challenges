184fd22f063e35cfef341486c7d7883a29117283
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-05-01 20:15:16 +0530

    Add files via upload

---
+++ recursion/PrintKpc.java
@@ -0,0 +1,30 @@
+package recursion;
+
+import java.io.*;
+import java.util.*;
+
+public class PrintKpc {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        printKPC(str, "");
+    }
+    static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx","yz"};
+    public static void printKPC(String ques, String ans) {
+        if(ques.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch=ques.charAt(0);
+        String roq=ques.substring(1);
+
+        String codesforch=codes[ch-'0'];
+        for(int i=0;i<codesforch.length();i++){
+            char cho=codesforch.charAt(i);
+            printKPC(roq,ans+cho);
+        }
+    }
+
+}
