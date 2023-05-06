779b29ef027e845fca2b94859e9f6470be8c57db
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-05-06 19:02:37 +0530

    Add files via upload

---
+++ dp/BuyAndSellStocksWithTransactionFeeInfiniteTransactionsAllowed.java
@@ -0,0 +1,41 @@
+package dp;
+import java.io.*;
+import java.util.*;
+
+public class BuyAndSellStocksWithTransactionFeeInfiniteTransactionsAllowed {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);;
+        int n=scn.nextInt();
+
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        int fee=scn.nextInt();
+
+        int obsp=-arr[0];
+        int ossp=0;
+
+        for(int i=1;i<n;i++){
+            int nbsp=0;
+            int nssp=0;
+
+            if(ossp-arr[i] >obsp){
+                nbsp=ossp-arr[i];
+            }else{
+                nbsp=obsp;
+            }
+            if(obsp+arr[i]-fee>ossp){
+                nssp=obsp+arr[i]-fee;
+            }else{
+                nssp=ossp;
+            }
+            obsp=nbsp;
+            ossp=nssp;
+        }
+        System.out.println(ossp);
+    }
+
+}
