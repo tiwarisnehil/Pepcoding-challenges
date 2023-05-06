8c4bd7092240aaf3f245575096c2134b783a1583
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-05-06 19:02:58 +0530

    Add files via upload

---
+++ dp/BuyAndSellStocksTwoTransactionsAllowed.java
@@ -0,0 +1,54 @@
+package dp;
+import java.io.*;
+import java.util.*;
+
+public class BuyAndSellStocksTwoTransactionsAllowed {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        int mpist=0;
+        int leastsf=arr[0];
+        int[]dpl=new int[arr.length];
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<leastsf){
+                leastsf=arr[i];
+            }
+            mpist=arr[i]-leastsf;
+            if(mpist>dpl[i-1]){
+                dpl[i]=mpist;
+            }else{
+                dpl[i]=dpl[i-1];
+            }
+        }
+
+        int mpibt=0;
+        int maxat=arr[arr.length-1];
+        int []dpr=new int[arr.length];
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]>maxat){
+                maxat=arr[i];
+            }
+            mpibt=maxat-arr[i];
+            if(mpibt>dpr[i+1]){
+                dpr[i]=mpibt;
+            }else{
+                dpr[i]=dpr[i+1];
+            }
+        }
+        int op=0;
+        for(int i=0;i<arr.length;i++){
+            if(dpl[i]+dpr[i]>op){
+                op=dpl[i]+dpr[i];
+            }
+        }
+        System.out.println(op);
+    }
+
+}
