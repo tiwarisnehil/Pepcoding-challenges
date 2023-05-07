edca185c479c15930a8b13edb9d23f8c620c36ed
Snehil Tiwari <86239745+tiwarisnehil@users.noreply.github.com>
2023-05-07 08:53:48 +0530

    Add files via upload

---
+++ stacks/BalancedBrackets.java
@@ -0,0 +1,55 @@
+package stacks;
+import java.io.*;
+import java.util.*;
+
+public class BalancedBrackets {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.nextLine();
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='(' || ch== '{' || ch=='['){
+                st.push(ch);
+            }
+            else if(ch==')'){
+                boolean val=handler(st,'(');
+                if(val==false){
+                    System.out.println(val);
+                    return;
+                }
+            }
+            else if(ch=='}'){
+                boolean val=handler(st,'{');
+                if(val==false){
+                    System.out.println(val);
+                    return;
+                }
+            }
+            else if(ch==']'){
+                boolean val=handler(st,'[');
+                if(val==false){
+                    System.out.println(val);
+                    return;
+                }
+            }
+        }
+        if(st.size()==0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+    public static boolean handler(Stack<Character>st,char corresoch){
+        if(st.size()==0){
+            return false;
+        }else if(st.peek()!=corresoch){
+            return false;
+        }else{
+            st.pop();
+            return true;
+        }
+    }
+
+}
